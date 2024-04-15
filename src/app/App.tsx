8e3439ebe5dcd892485e86fbb99a9f5a0b59e493
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-04-15 22:42:39 +0900

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -42,8 +42,7 @@ const location = useLocation();
         <HelpPage />
          </Route>
           <Route path="/">
-            < Test />
-         {/*    <HomePage /> */}
+            <HomePage />
           </Route>
         </Switch>
         <Footer/>
