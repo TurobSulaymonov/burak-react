7de854d3cd0e779372865873225b84f07502c429
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-04-24 03:36:41 +0900

    feat: create config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -13,67 +13,23 @@ import {createSelector} from "reselect";
 import {setPopularDishes} from "./slice";
 import { retrievePopularDishes} from "./selector";
 import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
 
 /* REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
-const popularDisheRetriver = createSelector(
+const popularDishesRetriever = createSelector(
   retrievePopularDishes,
   (popularDishes) => ({popularDishes})
 );
 
 export default function HomePage() {
   const {setPopularDishes} = actionDispatch(useDispatch());
-  const {popularDishes} = useSelector(popularDisheRetriver);
+  const {popularDishes} = useSelector(popularDishesRetriever);
  // Selector: Store => Data
 
- useEffect(() => {
-  return() => {
-    // Backend server date request  => Date
-   /*   const result = [
-      {
-          "_id": "6623ee2b3801c011e4c11d2e",
-          "productStatus": "PROCESS",
-          "productCollection": "DISH",
-          "productName": "Cutlet",
-          "productPrice": "9",
-          "productLeftCount": "60",
-          "productSize": "NORMAL",
-          "productVolume": 1,
-          "productDesc": "Cutlet",
-          "productImages": [
-              "uploads/products/47d4a797-2ed5-4e72-80d9-8a7f32d18230.jpg"
-          ],
-          "productViews": 0,
-          "createdAt": "2024-04-20T16:32:43.931Z",
-          "updatedAt": "2024-04-20T16:38:09.733Z",
-          "__v": 0
-      },
-      {
-          "_id": "6623edb33801c011e4c11d27",
-          "productStatus": "PROCESS",
-          "productCollection": "DISH",
-          "productName": "FR Kebab",
-          "productPrice": "10",
-          "productLeftCount": "100",
-          "productSize": "NORMAL",
-          "productVolume": 1,
-          "productDesc": "Fr Kebab",
-          "productImages": [
-              "uploads/products/29abdd78-13c5-4b0c-a1a0-322880b021cd.jpg"
-          ],
-          "productViews": 0,
-          "createdAt": "2024-04-20T16:30:43.835Z",
-          "updatedAt": "2024-04-20T16:38:15.231Z",
-          "__v": 0
-      },
-  ] */
-    // slice Date = Store
-    //@ts-ignore 
-    setPopularDishes();
-  };
- }, []);
+ useEffect(() => {}, []);
 
  // console.log("popularDishes:", popularDishes)
 
